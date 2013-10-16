3a3b6d18eedc97f8b77c3591b301503cbb3cc982
Christopher Maier <cjmaier2@illinois.edu>
2013-10-16 01:56:33 +0000

    import project

---
+++ mp2/module/.tmp_versions/tuxctl.mod
@@ -0,0 +1,2 @@
+/workdir/copies/mp2/module/tuxctl.ko
+/workdir/copies/mp2/module/tuxctl-ioctl.o /workdir/copies/mp2/module/tuxctl-ld.o
---
+++ mp2/module/.tuxctl.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/workdir/copies/mp2/module/tuxctl.ko := ld -m elf_i386 -m elf_i386 -r -o /workdir/copies/mp2/module/tuxctl.ko /workdir/copies/mp2/module/tuxctl.o /workdir/copies/mp2/module/tuxctl.mod.o
