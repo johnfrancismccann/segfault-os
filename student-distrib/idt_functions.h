970baaa2a5519da2e85a213bf653d092c0b47ef1
David Turner <dkturne2@illinois.edu>
2013-10-26 19:50:36 +0000

    clean up functions

--- student-distrib/idt_functions.h
+++ student-distrib/idt_functions.h
@@ -8,23 +8,23 @@
 
 #include "types.h"
 
-void do_idt_div_error();//uint32_t error_code, uint32_t stack_address);
-void do_idt_debug();//uint32_t error_code, uint32_t stack_address);
-void do_idt_nmi();//uint32_t error_code, uint32_t stack_address);
-void do_idt_breakpoint();//uint32_t error_code, uint32_t stack_address);
-void do_idt_overflow();//uint32_t error_code, uint32_t stack_address);
-void do_idt_bound();//uint32_t error_code, uint32_t stack_address);
-void do_idt_invalid_op();//uint32_t error_code, uint32_t stack_address);
-void do_idt_device_not_available();//uint32_t error_code, uint32_t stack_address);
-void do_idt_double_fault();//uint32_t error_code, uint32_t stack_address);
-void do_idt_coprocessor_segment_overrun();//uint32_t error_code, uint32_t stack_address);
-void do_idt_invalid_TSS();//uint32_t error_code, uint32_t stack_address);
-void do_idt_segment_not_present();//uint32_t error_code, uint32_t stack_address);
-void do_idt_stack_segment();//uint32_t error_code, uint32_t stack_address);
-void do_idt_general_protection();//uint32_t error_code, uint32_t stack_address);
-void do_idt_page_fault();//uint32_t error_code, uint32_t stack_address);
-void do_idt_coprocessor_error();//uint32_t error_code, uint32_t stack_address);
-void do_idt_alignment_check();//uint32_t error_code, uint32_t stack_address);
-void do_idt_system_call();//uint32_t error_code, uint32_t stack_address);
+void do_idt_div_error();
+void do_idt_debug();
+void do_idt_nmi();
+void do_idt_breakpoint();
+void do_idt_overflow();
+void do_idt_bound();
+void do_idt_invalid_op();
+void do_idt_device_not_available();
+void do_idt_double_fault();
+void do_idt_coprocessor_segment_overrun();
+void do_idt_invalid_TSS();
+void do_idt_segment_not_present();
+void do_idt_stack_segment();
+void do_idt_general_protection();
+void do_idt_page_fault();
+void do_idt_coprocessor_error();
+void do_idt_alignment_check();
+void do_idt_system_call();
 
 #endif
