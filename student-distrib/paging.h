3350137e5b9e367e7837364ce6bb23ca6cb406b8
John McCann <jmccann2@illinois.edu>
2013-10-27 09:51:37 +0000

    adding paging initializing. mostly working except for some unexplained page faults originating from dereferencing the last 3 bytes of the second 4MB chunk in memory

--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -1,14 +1,24 @@
 #ifndef PAGING_H
 #define	PAGING_H
 
-#define PAGE_DIR_SIZE 1024
-#define PAGE_TABLE_SIZE 1024
+#define PAGE_DIR_SIZE 			0x400
+#define PAGE_TABLE_SIZE 		0x400
+#define PAGE_SIZE_4K 				0x1000
+#define KERNEL_LOAD_POINT 	0x400000
+
+#define SET_PAGE_4MB				0x80
+#define SET_PAGE_RW					0x2
+#define SET_PAGE_PRES				0x1
 
 #define PG_TBL_ALIGN 0x1000
 #define PG_DIR_ALIGN 0x1000
 
-void init_paging();
-
+/*
+ * init_paging
+ *
+ *
+ */
+extern void init_paging();
 
 #endif
 
