2c638fa44154a183f3e232098fbcfd9888313369
David Turner <dkturne2@illinois.edu>
2013-11-02 21:13:34 +0000

    added RTC read/write basic functionality and kernel.c test passage for RTC frequency changes

--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -10,6 +10,8 @@
 #ifndef _RTC_H
 #define _RTC_H
 
+#include "types.h"
+
 //The RTC is IRQ_8.  In IDT, IRQs are entries 32:47
 #define RTC_IRQ_NUM 0x8
 #define RTC_IDT_NUM 0x28
@@ -18,8 +20,21 @@
 
 #define RTC_PORT 0x70
 
+//Frequency range for RTC
+#define MAXFREQ 1024
+#define MINFREQ 2
+#define BASEFREQ 32768
+
 void init_rtc();
 
 void rtc_idt_handle();
+
+int rtc_open();
+
+int rtc_read();
+
+int rtc_write(uint32_t freq);
+
+int rtc_close();
 
 #endif
