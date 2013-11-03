9801e448df6001551cadaa05d184717c80a76d77
cjmaier2 <cjamier2@illinois.edu>
2013-11-03 11:27:20 +0000

    terminal functions completed

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -14,60 +14,60 @@
  * term_open()
  *   DESCRIPTION: Initialize terminal
  *   INPUTS: none
- *   OUTPUTS: none
+ *   OUTPUTS: 0 on success, -1 on fail
  *   RETURN VALUE: none
  *   SIDE EFFECTS: none
  */
 int32_t term_open() {
 	
 	return 0; //indicates successful open
+
 }
 
 /*
  * term_close()
  *   DESCRIPTION: Close terminal
  *   INPUTS: none
- *   OUTPUTS: none
+ *   OUTPUTS: 0 on success, -1 on fail
  *   RETURN VALUE: none
  *   SIDE EFFECTS: none
  */
 int32_t term_close() {
 	
 	return 0; //indicates successful close
+
 }
 
 /*
  * term_read()
- *   DESCRIPTION: Copies buffer from kernel space (read_buf) to user space (pointer).
- *   INPUTS: buffer where character buffer form keyboard will be copied
- *   OUTPUTS: none
+ *   DESCRIPTION: Copies buffer from kernel space (terminal) to user space (read_tr).
+ *   INPUTS: pointer to which character buffer form keyboard will be copied
+ *   OUTPUTS: number of bytes read in
  *   RETURN VALUE: none
  *   SIDE EFFECTS: none
  */
-int32_t term_read(void* pointer) {	
+int32_t term_read(void* read_ptr) {	
 	int bytes_copied;
 
-	bytes_copied = get_read_buf(pointer);
+	bytes_copied = get_read_buf(read_ptr);
 
-	// void* read_buffer = (void*) get_read_buf(buf);
-	// if(read_buffer == -1) return -1; //indicates nothing has been typed
 	clear_read_buf();
 
-    //memcpy(buf, (void*) read_buffer, buf_idx+1);
-    //puts(pointer);
     return bytes_copied;
 }
 
-
 /*
  * term_write()
- *   DESCRIPTION: Copies buffer from user space to kernel space.
- *   INPUTS: buffer to be copied, number of bytes to copy
- *   OUTPUTS: none
+ *   DESCRIPTION: Copies buffer from user space (wrt_ptr) to kernel space (terminal).
+ *   INPUTS: pointer to buffer to be written
+ *   OUTPUTS: number of bytes written
  *   RETURN VALUE: none
  *   SIDE EFFECTS: none
  */
-void term_write(void* buf, int32_t n) {
-
-
+int32_t term_write(const void* wrt_ptr, int32_t nbytes) {
+	int32_t bytes_copied;
+	cli(); //disable interrupts so input printed cleanly
+	bytes_copied = print_write_buf(wrt_ptr, nbytes);
+	sti(); //re-enable interrupts
+	return bytes_copied;
 }
