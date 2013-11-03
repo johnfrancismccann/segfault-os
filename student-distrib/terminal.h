dcde212896cbce6b211464af96e88e0788fb6482
cjmaier2 <cjamier2@illinois.edu>
2013-11-03 01:05:29 +0000

    keyboard working except backspace scrolling for enter

---
+++ student-distrib/terminal.h
@@ -0,0 +1,12 @@
+/*
+ *  The terminal files CONTINUE THIS
+ */
+
+#ifndef _TERMINAL_H
+#define _TERMINAL_H
+
+void term_read(char* buf, unsigned long n);
+
+void term_read(char* buf, unsigned long n);
+
+#endif
