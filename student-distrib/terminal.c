8f432232bc8e4a08dd5eec314e749b8948e27d59
Christopher Maier <cjmaier2@illinois.edu>
2013-11-03 05:05:18 +0000

    keyboard working completely for sure now

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -17,9 +17,12 @@
  *   RETURN VALUE: none
  *   SIDE EFFECTS: none
  */
-void term_read(void* buf, unsigned long n){
+void term_read(char* read_buffer, void* buf, unsigned long n){
+	// if(buf == NULL) return; //make sure valid pointer
 
-    //memcpy( , buf, n)
+	// if(read_buf[buf_idx-1] == ENT_ASC) {
+ //    	memcpy(buf, (void*) read_buffer, n);
+	// }
 }
 
 
--- student-distrib/terminal.h
+++ student-distrib/terminal.h
@@ -5,8 +5,8 @@
 #ifndef _TERMINAL_H
 #define _TERMINAL_H
 
-void term_read(char* buf, unsigned long n);
+void term_read(char* read_buffer, char* buf, unsigned long n);
 
-void term_read(char* buf, unsigned long n);
+void term_write(char* buf, unsigned long n);
 
 #endif
