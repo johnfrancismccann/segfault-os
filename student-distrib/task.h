b7c3fab41cbce7c1d57e00f8a5b2a47e6829daa7
tigerzhua <wuwenyu2015@gmail.com>
2013-11-17 03:28:38 +0000

    syntx figured out

--- student-distrib/task.h
+++ student-distrib/task.h
@@ -4,7 +4,15 @@
 #include "paging.h"
 #include "fs.h"
 #include "lib.h"
-#include "types.h"
+#include "x86_desc.h"
+
+//task structures
+typedef struct
+{
+    tss_t task_tss;
+    seg_desc_t tss_dec;	
+} struct_task;
+//task structures end
 
 #endif
 
