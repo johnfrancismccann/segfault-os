d1e5e9b6f27b6914dedbe151b8e21c054f7f89ca
David Turner <dkturne2@illinois.edu>
2013-12-07 05:51:26 +0000

    forgot to add the error_codes.h file.  Wouldn't  compile without it\!

---
+++ student-distrib/error_codes.h
@@ -0,0 +1,12 @@
+/* Collection of error code definitions for any and
+ * all interractions requiring an error return value
+ */
+
+#ifndef _ERROR_CODES_H
+#define _ERROR_CODES_H
+
+#define PID_ERR		0
+#define GEN_ERROR 	0
+#define PROC_FULL 	1
+
+#endif
