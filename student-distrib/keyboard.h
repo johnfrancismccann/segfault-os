05ebd1aeabfa55f805fc3b535f82a1833dcfc8f5
John McCann <jmccann2@illinois.edu>
2013-12-08 16:20:42 +0000

    in gettting the scheduler to work with three terminals, the extra features had to be removed. currently, the os works fairly well with the exception of abnormal behavior from the execution of pingpong. also pressing control-L sometimes causes strange things to happen, but this is not to be unexpected, as I believe that would just cause the scheduler to preempt the currently executing process, not the one corresponding to the active display terminal.

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -10,25 +10,27 @@
 #ifndef _KEYBOARD_H
 #define _KEYBOARD_H
 
-#define NUM_TERMS 3 //number of available terminals
+#define TEXT_COLOR CYAN
+#define PROG_COLOR GREEN //program color
+#define CURSOR_COLOR GREEN
 
 //text color codes for text-mode VGA attribute byte (LT == light, DK == dark)
-#define BLACK       0
-#define BLUE        1
-#define GREEN       2
-#define CYAN        3
-#define RED         4
-#define MAGENTA     5
-#define BROWN       6
-#define LT_GREY     7
-#define DK_GREY     8
-#define LT_BLUE     9
-#define LT_GREEN    10
-#define LT_CYAN     11
-#define LT_RED      12
-#define LT_MAGENTA  13
-#define LT_BROWN    14
-#define WHITE       15
+#define BLACK 0
+#define BLUE 1
+#define GREEN 2
+#define CYAN 3
+#define RED 4
+#define MAGENTA 5
+#define BROWN 6
+#define LT_GREY 7
+#define DK_GREY 8
+#define LT_BLUE 9
+#define LT_GREEN 10
+#define LT_CYAN 11
+#define LT_RED 12
+#define LT_MAGENTA 13
+#define LT_BROWN 14
+#define WHITE 15
 
 #ifndef OFF
 #define OFF 0
@@ -38,61 +40,43 @@
 #define ON 1
 #endif
 
-/* Keyboard is IRQ 1, IDT number 33, port 0x60 */
+//Keyboard is IRQ 1, IDT number 33, port 0x60
 #define KBD_IRQ_NUM 1
 #define KBD_IDT_NUM 33
 #define KBD_PORT    0x60
 
-/* VGA ports */
-#define VGA_LOW     0x3D4 //low register of VGA port
-#define VGA_HIGH    0x3D5 //high register of VGA port
-
-/* miscellaneous keyboard macros */
-#define CAP_OFFSET  0x20 //offset between lower case and upper case ASCII value
-#define TAB_LEN     5 //set tab length to 5 spaces
-#define UP          0 //for upper scroll store (previous data)
-#define DN          1 //for lower scroll store (current data)
+#define VGA_LOW 0x3D4 //low register of VGA port
+#define VGA_HIGH 0x3D5 //high register of VGA port
+#define CAP_OFFSET 0x20 //offset between lower case and upper case ASCII value
+#define TAB_LEN 5 //set tab length to 5 spaces
 
 /* ASCII values */
-#define TAB_ASC     0x09 //tab key
-#define ENT_ASC     0x0A //enter key
-#define ONE_ASC     0x02 //#1
-#define ZERO_ASC    0x0B //#0
+#define TAB_ASC 0x09 //tab key
+#define ENT_ASC 0x0A //enter key
 
 /* scancodes */
-#define CTRL_PRS    0x1D //left/right control press
-#define CTRL_RLS    0x9D //left/right control release
-#define L_KEY       0x26 //'L' pressed
-#define B_SPACE     0x0E //backspace pressed
-#define TAB         0x0F //tab pressed
-#define ENTER       0x1C //enter pressed
-#define LSHIFT_PRS  0x2A //left shift pressed
-#define RSHIFT_PRS  0x36 //right shift pressed
-#define LSHIFT_RLS  0xAA //left shift released
-#define RSHIFT_RLS  0xB6 //right shift released
-#define CAPS        0x3A //caps lock pressed
-#define ALT_PRS     0x38
-#define ALT_RLS     0xB8
-#define PG_UP       0x49 //page up press
-#define PG_DN       0x51 //page down press
+#define CTRL_PRS 0x1D //left/right control press
+#define CTRL_RLS 0x9D //left/right control release
+#define L_KEY 0x26 //'L' pressed
+#define B_SPACE 0x0E //backspace pressed
+#define TAB 0x0F //tab pressed
+#define ENTER 0x1C //enter pressed
+#define LSHIFT_PRS 0x2A //left shift pressed
+#define RSHIFT_PRS 0x36 //right shift pressed
+#define LSHIFT_RLS 0xAA //left shift released
+#define RSHIFT_RLS 0xB6 //right shift released
+#define CAPS 0x3A //caps lock pressed
+#define ALT_PRS		0x38
+#define ALT_RLS		0xB8
 /* ALT_FX found on stanislavs.org/helppc/scan_codes.html */
-#define F1          0x3B 
-#define F2          0x3C
-#define F3          0x3D
-
-//for changing color scheme
-#define NUM_SCANCODE_OFFSET 1 //1 has scancode 0x02, 2 is 0x03, etc.
-
-//status bar macros
-#define STATUS_BAR_START   NUM_ROWS*NUM_COLS
-#define STATUS_BAR_END     (NUM_ROWS+1)*NUM_COLS-1 //also final terminal location
-#define TERM_INFO_START    NUM_ROWS*NUM_COLS //starting print index of terminal number information
-#define PROC_INFO_START    NUM_ROWS*NUM_COLS+18 //starting print index of process information
-#define BG_CLR_INFO_START  NUM_ROWS*NUM_COLS+37 //starting print index of background color information
-#define TXT_CLR_INFO_START NUM_ROWS*NUM_COLS+62 //starting print index of text color information
+ #define F1		0x3B 
+ #define F2		0x3C
+ #define F3		0x3D
 
 #define KBD_MAP_SIZE 256
-#define BUF_SIZE     128
+#define BUF_SIZE 128
+
+ #define NUM_TERMS 3
 
 #include "types.h"
 
@@ -100,24 +84,21 @@ void init_kbd();
 
 void kbd_handle();
 
-void update_cursor(uint32_t index);
+
+
+
+uint32_t get_act_ops_disp();
+
+//void check_scroll(int print_index);
+
+void set_act_ops_term(uint32_t term_index);
 
 int32_t get_read_buf(void* buf, int32_t bytes);
 
 void clear_read_buf();
 
+//int32_t get_act_ops_disp(uint8_t** act_ops_display);
+
 int32_t print_write_buf(const void* wrt_buf, int32_t bytes);
-
-char* strcat(char *dest, char *src); //standard string concatenation
-
-void print_status_bar(); //need to print status bar upon creation of new process
-
-uint32_t get_active_terminal();
-
-uint32_t get_act_ops_disp();
-
-void reset_print_inds();
-
-void remove_hw_cursor();
 
 #endif
