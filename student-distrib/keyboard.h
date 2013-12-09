97e07488d532c5961ecd9180c57b45da7c9774b3
David Turner <dkturne2@illinois.edu>
2013-12-09 05:11:11 +0000

    MAJOR BREAKTHROUGH: NEAR PERFECT FUNCTIONALITYclear && make clean && make dep && sudo make! Changed keyboard.c in check_scroll and other places where incorrect buffers were being modified leading to protection and page faults.

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -15,22 +15,22 @@
 #define CURSOR_COLOR GREEN
 
 //text color codes for text-mode VGA attribute byte (LT == light, DK == dark)
-#define BLACK 0
-#define BLUE 1
-#define GREEN 2
-#define CYAN 3
-#define RED 4
-#define MAGENTA 5
-#define BROWN 6
-#define LT_GREY 7
-#define DK_GREY 8
-#define LT_BLUE 9
-#define LT_GREEN 10
-#define LT_CYAN 11
-#define LT_RED 12
-#define LT_MAGENTA 13
-#define LT_BROWN 14
-#define WHITE 15
+#define BLACK       0
+#define BLUE        1
+#define GREEN       2
+#define CYAN        3
+#define RED         4
+#define MAGENTA     5
+#define BROWN       6
+#define LT_GREY     7
+#define DK_GREY     8
+#define LT_BLUE     9
+#define LT_GREEN    10
+#define LT_CYAN     11
+#define LT_RED      12
+#define LT_MAGENTA  13
+#define LT_BROWN    14
+#define WHITE       15
 
 #ifndef OFF
 #define OFF 0
@@ -53,6 +53,20 @@
 /* ASCII values */
 #define TAB_ASC 0x09 //tab key
 #define ENT_ASC 0x0A //enter key
+#define ONE_ASC     0x02 //#1
+#define ZERO_ASC    0x0B //#0
+/* Arrow Keys; NOTE QEMU is stupid, so the arrow keys show up
+ * as number pad keys.
+ *   NUM       KK0DE
+ *    8         48
+ *  4   6     4B  4D
+ *    2         50
+ */
+#define K_UP    0x48
+#define K_DOWN  0x50
+#define K_LEFT  0x4B
+#define K_RIGHT 0x4D
+
 
 /* scancodes */
 #define CTRL_PRS 0x1D //left/right control press
@@ -66,26 +80,40 @@
 #define LSHIFT_RLS 0xAA //left shift released
 #define RSHIFT_RLS 0xB6 //right shift released
 #define CAPS 0x3A //caps lock pressed
-#define ALT_PRS		0x38
-#define ALT_RLS		0xB8
+#define ALT_PRS     0x38
+#define ALT_RLS     0xB8
+#define PG_UP       0x49 //page up press
+#define PG_DN       0x51 //page down press
 /* ALT_FX found on stanislavs.org/helppc/scan_codes.html */
- #define F1		0x3B 
- #define F2		0x3C
- #define F3		0x3D
+#define F1     0x3B 
+#define F2     0x3C
+#define F3     0x3D
+
+
+//for changing color scheme
+#define NUM_SCANCODE_OFFSET 1 //1 has scancode 0x02, 2 is 0x03, etc.
+
+//status bar macros
+#define STATUS_BAR_START   NUM_ROWS*NUM_COLS
+#define STATUS_BAR_END     (NUM_ROWS+1)*NUM_COLS-1 //also final terminal location
+#define TERM_INFO_START    NUM_ROWS*NUM_COLS //starting print index of terminal number information
+#define PROC_INFO_START    NUM_ROWS*NUM_COLS+18 //starting print index of process information
+#define BG_CLR_INFO_START  NUM_ROWS*NUM_COLS+37 //starting print index of background color information
+#define TXT_CLR_INFO_START NUM_ROWS*NUM_COLS+62 //starting print index of text color information
+
 
 #define KBD_MAP_SIZE 256
 #define BUF_SIZE 128
 
- #define NUM_TERMS 3
+#define NUM_TERMS 3
+
+#define CMD_HIST_LEN 8
 
 #include "types.h"
 
 void init_kbd();
 
 void kbd_handle();
-
-
-
 
 uint32_t get_act_ops_disp();
 
