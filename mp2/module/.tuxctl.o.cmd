2c4666f47fd949f3a97580d5980c96d3dc2e597e
David Turner <dkturne2@illinois.edu>
2013-12-10 21:29:08 +0000

    removed unused mp1/mp2 files

--- mp2/module/.tuxctl.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/workdir/copies/mp2/module/tuxctl.o := ld -m elf_i386 -m elf_i386  -r -o /workdir/copies/mp2/module/tuxctl.o /workdir/copies/mp2/module/tuxctl-ioctl.o /workdir/copies/mp2/module/tuxctl-ld.o
